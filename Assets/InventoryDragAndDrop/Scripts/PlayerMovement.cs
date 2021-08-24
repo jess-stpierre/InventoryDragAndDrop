15e806a9aa5e63701317f2fa5f4754a55a87929a
Jessica <jessica.stpierregagne@gmail.com>
2021-08-24 18:04:30 -0400

    - Player movement is functional with old input system

---
+++ Assets/InventoryDragAndDrop/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Tooltip("Speed of movement for all axis")]
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody rb;
+    [Header(" ")]
+    [Header("Default movement is WASD, changeable below")]
+    [SerializeField] KeyCode forwards = KeyCode.W;
+    [SerializeField] KeyCode backwards = KeyCode.S;
+    [SerializeField] KeyCode right = KeyCode.D;
+    [SerializeField] KeyCode left = KeyCode.A;
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    /// <summary>
+    /// Based on X and Z values that we got from the inputs we than call the Move function for it to perform the movement for us.
+    /// </summary>
+    private void Movement()
+    {
+        float zDirection = DirectionOutput(forwards, backwards);
+        float xDirection = DirectionOutput(right, left);
+        Vector3 newDir = new Vector3(xDirection, 0f, zDirection);
+        Move(newDir.normalized);
+    }
+
+    /// <summary>
+    /// Create a new movement vector so we can move our rigidbody attached to the player.
+    /// </summary>
+    private void Move(Vector3 direction)
+    {
+        Vector3 move = direction * speed * Time.deltaTime;
+        rb.MovePosition(this.transform.position + move);
+    }
+
+    /// <summary>
+    /// Detects which direction we are going between positive and negative inputs.
+    /// </summary>
+    private float DirectionOutput(KeyCode posButton, KeyCode negButton)
+    {
+        float DirOutput = 0f;
+
+        if (Input.GetKey(posButton) && Input.GetKey(negButton)) DirOutput = 0f;
+        else if (Input.GetKey(posButton) || Input.GetKey(negButton))
+        {
+            if (Input.GetKey(posButton)) DirOutput = -1f;
+            if (Input.GetKey(negButton)) DirOutput = 1f;
+        }
+        else DirOutput = 0f;
+
+        return DirOutput;
+    }
+}
