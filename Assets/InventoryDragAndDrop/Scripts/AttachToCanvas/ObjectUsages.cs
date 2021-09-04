32b5f3c2cb9326bc0a24daf028f82345c75d00e9
Jessica <jessica.stpierregagne@gmail.com>
2021-09-04 18:57:15 -0400

    - Did object hot bar selection in script that is not named properly

--- Assets/InventoryDragAndDrop/Scripts/AttachToCanvas/ObjectUsages.cs
+++ Assets/InventoryDragAndDrop/Scripts/AttachToCanvas/ObjectUsages.cs
@@ -5,9 +5,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// RENAME SCRIPT TO OBJECTSELECTION INSTEAD
+/// </summary>
 public class ObjectUsages : MonoBehaviour
 {
+	[Header("The order of each element is important and associated with the itemKeys elements")]
 	[SerializeField] private List<GameObject> usageHotbar = new List<GameObject>();
+	[SerializeField] private GameObject cursor;
+	[Header("The order of each element is important and associated with the usageHotBar elements")]
+	[SerializeField] private List<KeyCode> itemKeys = new List<KeyCode>();
 
 	//1 - 5 hotbar or mouse scrolling to select next
+	private void Start()
+	{
+		cursor.transform.position = usageHotbar[0].transform.position;
+	}
+
+	private void Update()
+	{
+		for (int i = 0; i < usageHotbar.Count; i++)
+		{
+			Selection(i, i);
+		}
+	}
+
+	private void Selection(int j, int i)
+	{
+		if (Input.GetKeyDown(itemKeys[j])) cursor.transform.position = usageHotbar[i].transform.position;
+	}
+
+
 }
