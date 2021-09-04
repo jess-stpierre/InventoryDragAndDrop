78d3ee08624b996a73b7483689c5a7a98adaae08
Jessica <jessica.stpierregagne@gmail.com>
2021-09-04 13:14:23 -0400

    - Made the canvas persistant

---
+++ Assets/InventoryDragAndDrop/Scripts/UI/Toolbox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Anything a child of this gameObject will not be destroyed between levels/ scenes
+/// </summary>
+public class Toolbox : MonoBehaviour
+{
+    public static Toolbox Instance { get; private set; }
+
+    public GameObject canvas;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+
+        if (canvas != null)
+        {
+            var go3 = canvas;
+            go3.transform.parent = this.gameObject.transform;
+        }
+
+        if (canvas == null)
+        {
+            canvas = GameObject.FindGameObjectWithTag("Canvas");
+        }
+    }
+}
